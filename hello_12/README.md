a2323d99d9596ea25136993990fc961654af9197
hjh0915 <623299157@qq.com>
2023-02-02 15:38:05 +0800

    测试

---
+++ hello_12/README.md
@@ -0,0 +1,12 @@
+测试
+====
+可以不写在main函数中
+单独分离包
+
+命名
+====
+xxx_test.go
+
+执行
+====
+>>> go test -v xxx_test.go
---
+++ hello_12/word/word_test.go
@@ -0,0 +1,27 @@
+package word
+
+import "testing"
+
+func IsPalindrome(s string) bool {
+	for i := range s {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestPalindrome(t *testing.T) {
+	if !IsPalindrome("detartrated") {
+		t.Error(`IsPalindrome("detartrated") = false`)
+	}
+	if !IsPalindrome("kayak") {
+		t.Error(`IsPalindrome("kayak") = false`)
+	}
+}
+
+func TestNonPalindrome(t *testing.T) {
+	if IsPalindrome("palindrome") {
+		t.Error(`IsPalindrome("palindrome") = true`)
+	}
+}
